babebfe3fdd8dc635451b38750d1dde596e76cd8
Atif Mahmud <atifmahmud95@gmail.com>
2017-06-04 18:04:05 +0600

    Trying to work on polymorphism

--- Inheritance/src/Main.java
+++ Inheritance/src/Main.java
@@ -12,7 +12,25 @@ public class Main {
 		//Bird.privateMethod(); NOT VISIBLE
 		//System.out.println(Bird.privateField); NOT VISIBLE
 		Sparrow.wingFlap();
-		MySparrow.sayHello();
 		
+		Bird MyBird = new Bird();
+		Bird MyChidiya = new Sparrow();
+		polymorphicArgMethod(MyBird);
+		polymorphicArgMethod(MyChidiya);
+		
+		
+		Bird BirdArray[] = new Bird [4];
+		BirdArray[0] = new Sparrow();
+		
+		System.out.println("\nHope to see a sparrow!");
+		BirdArray[0].sayHello();
+	
 	}
+	
+	
+	// Demonstrate polymorphic argument
+	public static void polymorphicArgMethod(Bird bird){
+		bird.sayHello();
+	}
+			
 }
