ba1e24aabaa0f6d7fb3ea4eafd6fc4a2cf649348
Atif Mahmud <atifmahmud95@gmail.com>
2017-06-05 21:32:01 +0600

    JFrame

---
+++ GUIPractice/src/GUI.java
@@ -0,0 +1,24 @@
+import java.awt.FlowLayout; // for the screen layout
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class GUI extends JFrame{
+	
+	private JLabel myLabel;
+	
+	public GUI(){
+		
+		//This is for the window
+		super("This is the title of my window");
+		setLayout(new FlowLayout());
+		
+		//This is for the text
+		myLabel = new JLabel("This is the text"); // a label is essentially text
+		myLabel.setToolTipText("This will display on hover"); // this will display on hover
+		
+		//Add the label to window
+		add(myLabel);
+		
+	}
+
+}
---
+++ GUIPractice/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * This program is for practicing the GUI concepts learnt in thenewboston tutorials
+ */
+
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main (String [] args){
+		
+		GUI MyGUI = new GUI(); // instantiate
+		MyGUI.setSize(275, 180); // set dimensions
+		MyGUI.setVisible(true); // set visibility
+	}
+}
