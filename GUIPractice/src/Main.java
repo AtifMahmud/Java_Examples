33d50fb3824247f917208fe6ced38d63c241666d
Atif Mahmud <atifmahmud95@gmail.com>
2017-06-06 14:09:23 +0600

    Added Default close operation

--- GUIPractice/src/Main.java
+++ GUIPractice/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		
 		GUI MyGUI = new GUI(); // instantiate
 		MyGUI.setSize(500, 500); // set dimensions
+		MyGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exits when you click close
 		MyGUI.setVisible(true); // set visibility
 		
 	}
