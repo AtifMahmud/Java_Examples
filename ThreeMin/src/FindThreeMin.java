7f188dac0f8721be577dad500433f4b7e44d2936
Atif Mahmud <atifmahmud95@gmail.com>
2018-01-21 16:34:03 -0800

    Fixed

--- ThreeMin/src/FindThreeMin.java
+++ ThreeMin/src/FindThreeMin.java
@@ -1,36 +1,53 @@
 public class FindThreeMin {
 
     public static void main(String [] args){
-        int [] arr1 = {1,1,1};
+        int [] arr1 = {1,2,3};
         int [] arr2 = {10000,2,4,5,2,1,0};
+        int [] arr3 = {2,1,3,1000};
+        int [] arr4 = {9,8,7,1,2,3,0};
         threemin(arr1);
         threemin(arr2);
+        threemin(arr3);
+        threemin(arr4);
     }
 
     private static void threemin(int [] arr){
         int min1 = arr[0];
-        int min2 = Integer.MAX_VALUE;
-        int min3 = Integer.MAX_VALUE ;
+        int min2 = arr[1];
+        int min3 = arr[2];
+        int temp, index = 0;
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < min1) {
                 min1 = arr[i];
+                index = i;
             }
         }
+        temp = arr[index];
+        arr[index] = arr[0];
+        arr[0] = temp;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < min2 && arr[i] >= min1) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min2 && arr[i] > min1) {
                 min2 = arr[i];
+                index = i;
             }
         }
+        temp = arr[index];
+        arr[index] = arr[1];
+        arr[1] = temp;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < min3 && arr[i] >= min2) {
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] < min3 && arr[i] > min2) {
                 min3 = arr[i];
+                index = i;
             }
         }
+        temp = arr[index];
+        arr[index] = arr[2];
+        arr[2] = temp;
 
 
-        System.out.println(min1 + " " + min2 + " " + min3);
+        System.out.println(arr[0] + " " + arr[1] + " " + arr[2]);
     }
 }
